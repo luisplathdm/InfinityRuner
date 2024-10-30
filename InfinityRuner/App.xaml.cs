4b7497d968849193798737c517e153bc76eb02e8
UsainBolt <luisgustavoplathdisaro@gmail.com>
2024-10-30 13:25:24 -0300

    CUMITÃO VAZIO DE LEIII

---
+++ InfinityRuner/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace InfinityRuner;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ InfinityRuner/Platforms/Tizen/Main.cs
@@ -0,0 +1,16 @@
+using System;
+using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+
+namespace InfinityRuner;
+
+class Program : MauiApplication
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+	static void Main(string[] args)
+	{
+		var app = new Program();
+		app.Run(args);
+	}
+}
