a0149eac2745a88771316df1547b300b0f8d4d6b
usainboltmolhadinho <luisgustavoplathdisaro@gmail.com>
2024-11-22 11:04:25 -0300

    A cara não du tempo

---
+++ InfinityRuner/Inimigo.cs
@@ -0,0 +1,22 @@
+namespace InfinityRuner;
+public class Inimigo
+{
+    Image ImageView;
+
+    public Inimigo(Image A)
+    {
+        ImageView = A;
+    }
+    public void MoveX(double B)
+    {
+        ImageView.TranslationX -= B;
+    }
+    public double GetX ()
+    {
+        return ImageView.TranslationX;
+    }
+    public void Reset()
+    {
+        ImageView.TranslationX = 500;
+    }
+}
---
+++ InfinityRuner/Inimigos.cs
@@ -0,0 +1,17 @@
+namespace InfinityRuner;
+public class Inimigos
+{
+    List<Inimigo>Inimigos = new List<Inimigos>();
+    Inimigo atual = null;
+    double minX = 0;
+
+    public Inimigos (double A)
+    {
+        minX = A;
+    }
+    public void ADD (Inimigo A)
+    {
+        Inimigos.Add(A);
+        i
+    }
+}
